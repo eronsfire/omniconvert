2b685e325207f3d4d475b88efeac0c357786fb8a
eronsfire <willian14colaco@gmail.com>
2026-08-07 13:58:15 -0300

    Corrige argumento de build do flet

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pdf2image import convert_from_path
 import yt_dlp
 import requests
 
-VERSION_ATUAL = "0.6"
+VERSION_ATUAL = "0.7"
 REPO_GITHUB = "eronsfire/omniconvert"  # Seu usuário/repositório
 
 
